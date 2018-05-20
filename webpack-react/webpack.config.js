9a7a4706015c740092bf2c5e50079ed24e5d98ef
yami_beta <yami-beta@users.noreply.github.com>
2018-05-20 19:08:34 +0900

    :up: Use webpack-serve

--- webpack-react/webpack.config.js
+++ webpack-react/webpack.config.js
@@ -5,89 +5,81 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const path = require("path");
 
-module.exports = (env, argv) => {
-  let { mode } = argv;
-  let publicPath = "/";
+let mode = process.env.WEBPACK_SERVE ? "development": "production"
+let publicPath = "/";
 
-  return {
-    mode: "development",
-    entry: {
-      index: [path.join(__dirname, "src", "index.jsx")]
-    },
-    output: {
-      path: path.join(__dirname, "dist"),
-      filename: "[name].js",
-      publicPath
-    },
-    resolve: {
-      extensions: [".js", ".jsx"]
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          use: ["babel-loader"],
-          exclude: /node_modules/
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                sourceMap: mode === "development"
-              }
-            },
-            {
-              loader: "css-loader",
-              options: {
-                importLoaders: 2,
-                modules: true,
-                localIdentName: "[name]--[local]--[hash:base64]",
-                sourceMap: mode === "development"
-              }
-            },
-            {
-              loader: "postcss-loader",
-              options: {
-                sourceMap: mode === "development"
-              }
-            },
-            {
-              loader: "sass-loader",
-              options: {
-                sourceMap: mode === "development"
-              }
+module.exports = {
+  mode,
+  entry: {
+    index: [path.join(__dirname, "src", "index.jsx")]
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              sourceMap: mode === "development"
             }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: "[name].css"
-      }),
-      new HtmlWebpackPlugin({
-        template: path.join(__dirname, "src", "index.html")
-      })
-    ],
-    optimization: {
-      minimizer: [
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            output: {
-              comments: /^\**!|@preserve|@license|@cc_on/
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              modules: true,
+              localIdentName: "[name]--[local]--[hash:base64]",
+              sourceMap: mode === "development"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: mode === "development"
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: mode === "development"
             }
           }
-        }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
-    },
-    devServer: {
-      contentBase: path.join(__dirname, "dist"),
-      publicPath,
-      historyApiFallback: true,
-      watchContentBase: true
-    }
-  };
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html")
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: /^\**!|@preserve|@license|@cc_on/
+          }
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
 };
